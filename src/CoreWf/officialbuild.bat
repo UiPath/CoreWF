da308d64b6096cc7ca134321c291a711c5d3b694
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2019-02-28 14:01:59 +0200

    relative paths in nuspec

--- src/CoreWf/officialbuild.bat
+++ src/CoreWf/officialbuild.bat
@@ -1,2 +1,2 @@
 dotnet msbuild System.Activities.csproj /p:Configuration=Release /p:OfficialBuild=true /t:Rebuild
-dotnet pack /p:Configuration=Release /p:NuspecFile=CoreWf.nuspec /p:NuspecBasePath=.\pack /p:OfficialBuild=true
+dotnet pack /p:Configuration=Release /p:NuspecFile=CoreWf.nuspec /p:NuspecBasePath=. /p:OfficialBuild=true
