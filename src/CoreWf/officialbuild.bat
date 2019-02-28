0f08467b378619e44afb1a044f9f916f1f35be41
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2019-02-28 13:41:12 +0200

    the csproj was renamed

--- src/CoreWf/officialbuild.bat
+++ src/CoreWf/officialbuild.bat
@@ -1,2 +1,2 @@
-dotnet msbuild CoreWf.csproj /p:Configuration=Release /p:OfficialBuild=true /t:Rebuild
+dotnet msbuild System.Activities.csproj /p:Configuration=Release /p:OfficialBuild=true /t:Rebuild
 dotnet pack /p:Configuration=Release /p:NuspecFile=CoreWf.nuspec /p:NuspecBasePath=.\pack /p:OfficialBuild=true
