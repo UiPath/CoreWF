07f3e96416dbd899038e6cd90e931ce0eab1a572
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2020-01-14 10:40:21 +0200

    cosmetic

---
+++ src/System.Xaml/MonoTODOAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MonoTODOAttribute : Attribute
+{
+	public string Message { get; private set; }
+
+	public MonoTODOAttribute (string message = null)
+	{
+		Message = message;
+	}
+}
+
