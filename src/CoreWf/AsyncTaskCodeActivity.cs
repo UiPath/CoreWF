d32bdefc19b300225723e6e7b31a69d6f62300a6
Foxtrek_64 <foxtrek64@gmail.com>
2021-04-29 03:39:37 -0500

    Add AsyncTaskCodeActivity and VoidResult

---
+++ src/CoreWf/AsyncTaskCodeActivity.cs
@@ -0,0 +1,160 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Activities
+{
+    public abstract class AsyncTaskCodeActivity : AsyncCodeActivity
+    {
+        protected sealed override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
+        {
+            var cts = new CancellationTokenSource();
+            context.UserState = cts;
+
+            var task = ExecuteAsync(context, cts.Token);
+
+#if NET5_0_OR_GREATER
+            // Generic TaskCompletionSource is available starting in .NET 5
+            var tcs = new TaskCompletionSource(state);
+
+#else
+            // VoidResult allows us to simulate a void return type.
+            var tcs = new TaskCompletionSource<VoidResult>(state)
+#endif
+
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    tcs.TrySetException(t.Exception!.InnerExceptions); // If it's faulted, there was an exception.
+                }
+                else if (t.IsCanceled)
+                {
+                    tcs.TrySetCanceled();
+                }
+                else
+                {
+#if NET5_0_OR_GREATER
+                    tcs.TrySetResult();
+#else
+                    tcs.TrySetResult(default);
+#endif
+                }
+
+                callback?.Invoke(tcs.Task);
+            }, cts.Token);
+
+            return tcs.Task;
+        }
+
+        protected sealed override void EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
+        {
+            var task = (Task) result;
+            try
+            {
+                task.Wait();
+            }
+            catch (TaskCanceledException)
+            {
+                if (context.IsCancellationRequested)
+                {
+                    context.MarkCanceled();
+                }
+
+                throw;
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.Flatten().InnerExceptions)
+                {
+                    if (ex is not TaskCanceledException)
+                        return;
+
+                    if (context.IsCancellationRequested)
+                        context.MarkCanceled();
+                }
+
+                throw;
+            }
+        }
+
+        protected sealed override void Cancel(AsyncCodeActivityContext context)
+        {
+            var cts = (CancellationTokenSource)context.UserState;
+            cts.Cancel();
+        }
+
+        protected abstract Task ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken);
+    }
+
+    public abstract class AsyncTaskCodeActivity<TResult> : AsyncCodeActivity<TResult>
+    {
+        protected sealed override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
+        {
+            var cts = new CancellationTokenSource();
+            context.UserState = cts;
+            var task = ExecuteAsync(context, cts.Token);
+            var completionSource = new TaskCompletionSource<TResult>(state);
+
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    completionSource.TrySetException(t.Exception!.InnerExceptions); // If it's faulted, there was an exception.
+                }
+                else if (t.IsCanceled)
+                {
+                    completionSource.TrySetCanceled();
+                }
+                else
+                {
+                    completionSource.TrySetResult(t.Result);
+                }
+
+                callback?.Invoke(completionSource.Task);
+            }, cts.Token);
+
+            return completionSource.Task;
+        }
+
+        protected sealed override TResult EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
+        {
+            var task = (Task<TResult>) result;
+            try
+            {
+                return task.Result;
+            }
+            catch (OperationCanceledException)
+            {
+                if (context.IsCancellationRequested)
+                {
+                    context.MarkCanceled();
+                }
+
+                throw;
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.Flatten().InnerExceptions)
+                {
+                    if (ex is not OperationCanceledException) 
+                        continue;
+
+                    if (context.IsCancellationRequested)
+                    {
+                        context.MarkCanceled();
+                    }
+                }
+
+                throw;
+            }
+        }
+
+        protected sealed override void Cancel(AsyncCodeActivityContext context)
+        {
+            var cts = (CancellationTokenSource) context.UserState;
+            cts.Cancel();
+        }
+
+        protected abstract Task<TResult> ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken);
+    }
+}
---
+++ src/CoreWf/VoidResult.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Activities
+{
+    /// <summary>
+    /// Represents a void type, since <see cref="System.Void"/> is not a valid return type in C#.
+    /// </summary>
+    /// <remarks>Based on the Unit type from MediatR.</remarks>
+    internal readonly struct VoidResult : IEquatable<VoidResult>, IComparable<VoidResult>, IComparable
+    {
+        private static readonly VoidResult value = new();
+
+        /// <summary>
+        /// Default and only value of the <see cref="VoidResult"/> type.
+        /// </summary>
+        public static ref readonly VoidResult Value => ref value;
+
+        /// <summary>
+        /// Returns a task which wraps <see cref="Value"/>.
+        /// </summary>
+        public static Task<VoidResult> VoidTask { get; } = Task.FromResult(value);
+
+        /// <inheritdoc />
+        public int CompareTo(VoidResult other) => 0;
+
+        /// <inheritdoc />
+        int IComparable.CompareTo(object obj) => 0;
+
+        /// <inheritdoc />
+        public override int GetHashCode() => 0;
+
+        /// <inheritdoc />
+        public bool Equals(VoidResult other) => true;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is VoidResult;
+
+        /// <summary>
+        /// Determines whether the <paramref name="first"/> object is equal to the <paramref name="second"/> object.
+        /// </summary>
+        /// <param name="first">The first object.</param>
+        /// <param name="second">The second object.</param>
+        /// <c>true</c> if the <paramref name="first"/> object is equal to the <paramref name="second" /> object; otherwise, <c>false</c>.
+        public static bool operator ==(VoidResult first, VoidResult second) => first.Equals(second);
+
+        /// <summary>
+        /// Determines whether the <paramref name="first"/> object is equal to the <paramref name="second"/> object.
+        /// </summary>
+        /// <param name="first">The first object.</param>
+        /// <param name="second">The second object.</param>
+        /// <c>true</c> if the <paramref name="first"/> object is equal to the <paramref name="second" /> object; otherwise, <c>false</c>.
+        public static bool operator !=(VoidResult first, VoidResult second) => !first.Equals(second);
+
+        /// <inheritdoc />
+        public override string ToString() => "()";
+    }
+}
