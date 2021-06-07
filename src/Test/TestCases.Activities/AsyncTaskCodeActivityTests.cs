289b912dd9409cf92e3d7295174d7cc9a17a0eda
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2021-06-07 17:12:41 +0300

    cancellation test

--- src/Test/TestCases.Activities/AsyncTaskCodeActivityTests.cs
+++ src/Test/TestCases.Activities/AsyncTaskCodeActivityTests.cs
@@ -14,28 +14,43 @@ namespace TestCases.Activities
         [Fact]
         public void ShouldReturnVoidResult()
         {
-            var genericActivity = new AsyncTaskActivity<object>(Task.FromResult<object>(null));
+            var genericActivity = new AsyncTaskActivity<object>(_=>Task.FromResult<object>(null));
 
             object vr1 = null;
             object vr2 = WorkflowInvoker.Invoke<object>(genericActivity);
 
             Assert.Equal(vr1, vr2);
         }
-
         [Fact]
         public void ShouldThrow()
         {
-            Activity activity = new AsyncTaskActivity(()=>Task.FromException(new InvalidOperationException("@")));
+            Activity activity = new AsyncTaskActivity(_ => Task.FromException(new InvalidOperationException("@")));
             new Action(() => WorkflowInvoker.Invoke(activity)).ShouldThrow<InvalidOperationException>().Message.ShouldBe("@");
         }
-
+        [Fact]
+        public async Task ShouldCancel()
+        {
+            Activity activity = new AsyncTaskActivity(token=> Task.Delay(Timeout.Infinite, token));
+            var invoker = new WorkflowInvoker(activity);
+            var taskCompletionSource = new TaskCompletionSource();
+            InvokeCompletedEventArgs args = null;
+            invoker.InvokeCompleted += (sender, localArgs) =>
+            {
+                args = localArgs;
+                taskCompletionSource.SetResult();
+            };
+            invoker.InvokeAsync(invoker);
+            invoker.CancelAsync(invoker);
+            await taskCompletionSource.Task;
+            args.Error.ShouldBeOfType<WorkflowApplicationAbortedException>();
+        }
         [Theory]
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(3)]
         public void ShouldReturnConstantResult(int value)
         {
-            var activity = new AsyncTaskActivity<int>(async()=>
+            var activity = new AsyncTaskActivity<int>(async _=>
             {
                 await Task.Yield();
                 return value;
@@ -44,7 +59,6 @@ namespace TestCases.Activities
 
             Assert.Equal(value, result);
         }
-
         [Fact]
         public void ShouldWriteCorrectString()
         {
@@ -52,7 +66,7 @@ namespace TestCases.Activities
 
             using var memory = new MemoryStream();
 
-            Activity activity = new AsyncTaskActivity(async() =>
+            Activity activity = new AsyncTaskActivity(async _ =>
             {
                 using var writer = new StreamWriter(memory);
                 writer.Write(stringToWrite);
@@ -68,34 +82,14 @@ namespace TestCases.Activities
     }
     public class AsyncTaskActivity : AsyncTaskCodeActivity
     {
-        private readonly Task _task;
-
-        public AsyncTaskActivity(Task task)
-        {
-            _task = task;
-        }
-
-        public AsyncTaskActivity(Func<Task> action) : this(action())
-        {
-        }
-
-        public override Task ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken)
-            => _task;
+        private readonly Func<CancellationToken, Task> _action;
+        public AsyncTaskActivity(Func<CancellationToken, Task> action) => _action = action;
+        public override Task ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken) => _action(cancellationToken);
     }
-
     public class AsyncTaskActivity<TResult> : AsyncTaskCodeActivity<TResult>
     {
-        private readonly Task<TResult> _task;
-        public AsyncTaskActivity(Task<TResult> task)
-        {
-            _task = task;
-        }
-
-        public AsyncTaskActivity(Func<Task<TResult>> func) : this(func())
-        {
-        }
-
-        public override Task<TResult> ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken)
-            => _task;
+        private readonly Func<CancellationToken, Task<TResult>> _action;
+        public AsyncTaskActivity(Func<CancellationToken, Task<TResult>> action) => _action = action;
+        public override Task<TResult> ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken) => _action(cancellationToken);
     }
 }
