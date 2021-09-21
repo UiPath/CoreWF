84e4c3d1a48de9eb8ce7d5e74885f9a4c052b34e
Gabriela Lungu <gabriela.lungu@uipath.com>
2021-09-21 19:04:06 +0300

    added unit test

---
+++ src/Test/TestCases.Workflows/DataContractSerializerTests.cs
@@ -0,0 +1,35 @@
+﻿using Shouldly;
+using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Activities.XamlIntegration;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace TestCases.Workflows
+{
+    public class DataContractSerializerTests
+    {
+        [Fact]
+        public void VerifyCompiledLocationSerialized()
+        {
+            var activityContext = new ActivityContext();
+            var activity = new Sequence();
+            activity.InitializeAsRoot(null);
+            activityContext.Reinitialize(new ActivityInstance(activity), null, activity, 0);
+
+            var compiledLocation = new CompiledLocation<string>(getMethod: null,
+                setMethod: null,
+                locationReferences: new List<LocationReference>() { new RuntimeArgument(nameof(VerifyCompiledLocationSerialized), typeof(string), ArgumentDirection.Out) },
+                locations: null,
+                expressionId: 0,
+                compiledRootActivity: activity,
+                currentActivityContext: activityContext);
+
+            using MemoryStream stream = new MemoryStream();
+            new Action(() => new DataContractSerializer(typeof(CompiledLocation<string>)).WriteObject(stream, compiledLocation)).ShouldNotThrow();
+        }
+    }
+}
