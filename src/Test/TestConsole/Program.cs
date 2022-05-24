1fda61cc7b92ca182f23996523ac09cb16078f27
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-05-24 10:49:58 +0300

    w

--- src/Test/TestConsole/Program.cs
+++ src/Test/TestConsole/Program.cs
@@ -21,7 +21,7 @@ namespace TestConsole
         static void Main()
         {
             var stopWatch = System.Diagnostics.Stopwatch.StartNew();
-            WorkflowInvoker.Invoke(new TestDelay { Duration = TimeSpan.FromSeconds(1) });
+            WorkflowInvoker.Invoke(new TestDelay());
             stopWatch.Stop();
             Console.WriteLine(stopWatch.Elapsed.TotalSeconds);
             return;
@@ -40,78 +40,19 @@ namespace TestConsole
             new JustInTimeExpressions().SalaryCalculation();
         }
     }
-    [ContentProperty("Duration")]
-    public sealed class TestDelay : NativeActivity
+    public class TestDelay : AsyncTaskNativeActivity
     {
-        private static readonly Func<TimerExtension> getDefaultTimerExtension = new Func<TimerExtension>(GetDefaultTimerExtension);
-        private readonly Variable<Bookmark> _timerBookmark;
-        private readonly WriteLine _writeLine = new() { Text = "XXXXXXXXXXXXXXX" };
-
-        public TestDelay()
-            : base()
-        {
-            _timerBookmark = new Variable<Bookmark>();
-        }
-
-        [RequiredArgument]
-        [DefaultValue(null)]
-        public InArgument<TimeSpan> Duration { get; set; }
-
-        protected override bool CanInduceIdle => true;
-
+        WriteLine _writeLine = new() { Text = "XXXXXXXXXXXXXXXXXX" };
         protected override void CacheMetadata(NativeActivityMetadata metadata)
         {
-            RuntimeArgument durationArgument = new RuntimeArgument("Duration", typeof(TimeSpan), ArgumentDirection.In, true);
-            metadata.Bind(Duration, durationArgument);
+            base.CacheMetadata(metadata);
             metadata.AddChild(_writeLine);
-            metadata.SetArgumentsCollection(new Collection<RuntimeArgument> { durationArgument });
-            metadata.AddImplementationVariable(_timerBookmark);
-            metadata.AddDefaultExtensionProvider(getDefaultTimerExtension);
         }
-
-        private static TimerExtension GetDefaultTimerExtension() => new DurableTimerExtension();
-
-        protected override void Execute(NativeActivityContext context)
+        protected override async Task<Action<NativeActivityContext>> ExecuteAsync(NativeActivityContext context, CancellationToken cancellationToken)
         {
-            TimeSpan duration = Duration.Get(context);
-
-            if (duration == TimeSpan.Zero)
-            {
-                return;
-            }
-
-            TimerExtension timerExtension = GetTimerExtension(context);
-            Bookmark bookmark = context.CreateBookmark();
-            timerExtension.RegisterTimer(duration, bookmark);
-            _timerBookmark.Set(context, bookmark);
             context.ScheduleActivity(_writeLine);
-        }
-
-        protected override void Cancel(NativeActivityContext context)
-        {
-            Bookmark timerBookmark = _timerBookmark.Get(context);
-            TimerExtension timerExtension = GetTimerExtension(context);
-            timerExtension.CancelTimer(timerBookmark);
-            context.RemoveBookmark(timerBookmark);
-            context.MarkCanceled();
-        }
-
-        protected override void Abort(NativeActivityAbortContext context)
-        {
-            Bookmark timerBookmark = _timerBookmark.Get(context);
-            // The bookmark could be null in abort when user passed in a negative delay as a duration
-            if (timerBookmark != null)
-            {
-                TimerExtension timerExtension = GetTimerExtension(context);
-                timerExtension.CancelTimer(timerBookmark);
-            }
-            base.Abort(context);
-        }
-
-        private TimerExtension GetTimerExtension(ActivityContext context)
-        {
-            TimerExtension timerExtension = context.GetExtension<TimerExtension>();
-            return timerExtension;
+            await Task.Delay(1000);
+            return _ => { };
         }
     }
     public abstract class AsyncTaskNativeActivity : NativeActivity
