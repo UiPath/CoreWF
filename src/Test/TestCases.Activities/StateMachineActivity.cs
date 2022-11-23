63bc965cb5c8edef179d9a1cb9a101b80adc7170
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-11-23 12:40:33 +0200

    p

--- src/Test/TestCases.Activities/StateMachineActivity.cs
+++ src/Test/TestCases.Activities/StateMachineActivity.cs
@@ -11,7 +11,8 @@ public class StateMachineActivity
         State state1 = new() { Entry = new WriteLine { Text = "state1" }, Transitions = { new() { To = finalState } } };
         State state2 = new() { Entry = new WriteLine { Text = "state2" }, Transitions = { new() { To = finalState } } };
         State initialState = new() { Entry = new WriteLine { Text = "initialState" }, 
-            Transitions = { new() { To = state1, Condition = true }, new() { To = state2, Condition = true } } };
+            Transitions = { new() { Action = new WriteLine { Text = "t1" }, To = state1, Condition = true }, 
+                                  new() { Action = new WriteLine { Text = "t2" }, To = state2, Condition = true } } };
         StateMachine stateMachine = new() { InitialState = initialState, States = { initialState, state1, state2, finalState } };
         WorkflowInvoker.Invoke(stateMachine);
     }
