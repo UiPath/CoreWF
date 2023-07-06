0592fd5e57307742c4f09da48a9c21b16ba7d8d1
Gabriela Lungu <gabriela.lungu@uipath.com>
2023-07-06 19:46:25 +0300

    added one more test scenario

--- src/Test/TestCases.Workflows/ValidationExtensionsTests.cs
+++ src/Test/TestCases.Workflows/ValidationExtensionsTests.cs
@@ -20,7 +20,7 @@ public class ValidationExtensionsTests
         var seq = new Sequence();
         for (var j = 0; j < 10000; j++)
         {
-            seq.Activities.Add(new ActivityWithValidationError());
+            seq.Activities.Add(new ActivityWithValidationExtension());
         }
 
         var result = ActivityValidationServices.Validate(seq, _useValidator);
@@ -35,18 +35,27 @@ public class ValidationExtensionsTests
         {
             Activities =
             {
+                new ActivityWithValidationExtension(),
                 new ActivityWithValidationError(),
                 new WriteLine { Text = new InArgument<string>(new CSharpValue<string>("var1")) }
             }
         };
 
         var result = ActivityValidationServices.Validate(seq, _useValidator);
-        result.Errors.Count.ShouldBe(2);
-        result.Errors.First().Message.ShouldContain("The name 'var1' does not exist in the current context");
-        result.Errors.Last().Message.ShouldContain(nameof(MockValidationExtension));
+        result.Errors.Count.ShouldBe(3);
+        result.Errors.ShouldContain(error => error.Message.Contains(nameof(ActivityWithValidationError)));
+        result.Errors.ShouldContain(error => error.Message.Contains(nameof(MockValidationExtension)));
+        result.Errors.ShouldContain(error => error.Message.Contains("The name 'var1' does not exist in the current context"));
     }
 
     class ActivityWithValidationError : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context) => throw new NotImplementedException();
+
+        protected override void CacheMetadata(CodeActivityMetadata metadata) => metadata.AddValidationError(nameof(ActivityWithValidationError));
+    }
+
+    class ActivityWithValidationExtension : CodeActivity
     {
         protected override void Execute(CodeActivityContext context) => throw new NotImplementedException();
 
