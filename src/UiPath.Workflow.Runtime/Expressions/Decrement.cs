13ddf5c1b7516b690b66cbca11644a64d832bc03
Devin Duanne <dduanne@tafs.com>
2023-11-16 15:42:13 -0600

    Fix license headers

--- src/UiPath.Workflow.Runtime/Expressions/Decrement.cs
+++ src/UiPath.Workflow.Runtime/Expressions/Decrement.cs
@@ -1,24 +1,5 @@
-﻿//
-//  Decrement.cs
-//
-//  Author:
-//       Devin Duanne <dduanne@tafs.com>
-//
-//  Copyright (c) TAFS, LLC.
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+﻿// This file is part of Core WF which is licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
 
 using System.Activities.Validation;
 using System.Linq.Expressions;
