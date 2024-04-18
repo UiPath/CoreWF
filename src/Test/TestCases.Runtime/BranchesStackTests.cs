d3a1bdc0c2131bb15d42456cf661ef9706716e8a
Mihai Radu <mihainradu@gmail.com>
2024-04-18 20:46:50 +0300

    prepare ParallelTrackingToBeUsedInFlowchart

---
+++ src/Test/TestCases.Runtime/BranchesStackTests.cs
@@ -0,0 +1,78 @@
+﻿using Shouldly;
+using System;
+using System.Activities;
+using System.Activities.ParallelTracking;
+using System.Activities.Statements;
+using System.Linq;
+using WorkflowApplicationTestExtensions;
+using Xunit;
+
+namespace TestCases.Runtime;
+
+public class BranchesStackTests
+{
+    private static void Run(Activity activity) =>
+        new WorkflowApplication(activity).RunUntilCompletion();
+    public class TestCodeActivity(Action<CodeActivityContext> onExecute) : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            onExecute(context);
+        }
+    }
+    private static Activity Test(Action<CodeActivityContext> onExecute)
+    => new SuspendingWrapper(new TestCodeActivity(onExecute));
+
+    [Fact]
+    public void Push()
+    {
+        var level1 = new BranchesStack().Push();
+        var level2 = level1.Push();
+        var level3 = level2.Push(); 
+        level2.BranchesStackString.ShouldStartWith(level1.BranchesStackString);
+        level3.BranchesStackString.ShouldStartWith(level2.BranchesStackString);
+        var l3Splits = level3.BranchesStackString.Split('.');
+        l3Splits.Length.ShouldBe(3);
+        l3Splits.First().ShouldBe(level1.BranchesStackString);
+    }
+
+    [Fact]
+    public void ReadAndSave()
+    {
+        BranchesStack level1 = default;
+        BranchesStack level2 = default;
+        Run(new Sequence
+        {
+            Activities = {
+            Test(context =>
+            {
+                var sequenceActivityInstance = context.CurrentInstance.Parent.Parent;
+                sequenceActivityInstance.MarkNewParallelBranch();
+
+            }),
+            Test(context =>
+            {
+                var extensionLevel1 = context.GetCurrentParallelBranchId();
+                level1 = BranchesStack.ReadFrom(context.CurrentInstance);
+                level1.BranchesStackString.ShouldBe(extensionLevel1);
+            }),
+            Test(context =>
+            {
+                level2 = level1.Push();
+                level2.SaveTo(context.CurrentInstance);
+                var readLevel2 = BranchesStack.ReadFrom(context.CurrentInstance);
+                readLevel2.ShouldBe(level2);
+                context.CurrentInstance.GetCurrentParallelBranchId().ShouldBe(level2.BranchesStackString);
+            }),
+            Test(context =>
+            {
+                var readLevel = BranchesStack.ReadFrom(context.CurrentInstance);
+                var extensionLevel = context.GetCurrentParallelBranchId();
+                readLevel.BranchesStackString.ShouldBe(level1.BranchesStackString);
+                extensionLevel.ShouldBe(level1.BranchesStackString);
+            }),
+        }
+        });
+    }
+}
+
--- src/UiPath.Workflow.Runtime/ParallelTrackingExtensions.cs
+++ src/UiPath.Workflow.Runtime/ParallelTrackingExtensions.cs
@@ -1,27 +1,46 @@
 ﻿namespace System.Activities.ParallelTracking;
 
+[DataContract]
+public struct BranchesStack
+{
+    internal const string BranchIdPropertyName = "__ParallelBranchId";
+
+    [DataMember]
+    internal string BranchesStackString { get; init; }
+
+    public readonly BranchesStack Push() => new() { BranchesStackString = $"{BranchesStackString}.{Guid.NewGuid():N}".Trim('.') };
+
+    public static BranchesStack ReadFrom(ActivityInstance instance) =>
+        new()
+        {
+            BranchesStackString =
+            instance.PropertyManager?.GetPropertyAtCurrentScope(BranchIdPropertyName) as string
+        };
+
+    public readonly void SaveTo(ActivityInstance instance)
+    {
+        new ExecutionProperties(null, instance, instance.PropertyManager)
+            .Add(BranchIdPropertyName, this.BranchesStackString, true, false);
+    }
+}
+
 /// <summary>
 /// This feature introduces a context.GetCurrentParallelId() queryable from an activity,
 /// which should identify a "parallelism" branch on which the activity executes.
-/// Only containers which schedule concurrently (Parallel, ParallelForEach, Pick)
+/// Only containers which schedule concurrently (Parallel, ParallelForEach, Pick, and Flowchart with Split nodes)
 /// would induce a change of the background parallel id.
 /// The initial parallel id, before using the above containers, is null.
 /// </summary>
 public static class ParallelTrackingExtensions
 {
-    public const string BranchIdPropertyName = "__ParallelBranchId";
-
     public static ActivityInstance MarkNewParallelBranch(this ActivityInstance instance)
     {
-        var properties = new ExecutionProperties(null, instance, instance.PropertyManager);
-        var parentId = properties.Find(BranchIdPropertyName) as string;
-        properties.Add(BranchIdPropertyName, $"{parentId}.{Guid.NewGuid():N}".Trim('.'), true, false);
-
+        BranchesStack.ReadFrom(instance).Push().SaveTo(instance);
         return instance;
     }
 
     public static string GetCurrentParallelBranchId(this ActivityInstance instance) =>
-        instance.PropertyManager?.GetPropertyAtCurrentScope(BranchIdPropertyName) as string;
+        BranchesStack.ReadFrom(instance).BranchesStackString;
 
     public static ActivityInstance MarkNewParallelBranch(this ActivityContext context) =>
          context.CurrentInstance?.MarkNewParallelBranch();
