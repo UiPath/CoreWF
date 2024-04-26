def06bbf72e9020e23b6d76ac71c1d7e7b0f1d31
Alexandru-Ionut Mustata <alexandru.mustata@uipath.com>
2024-04-26 14:19:41 +0300

    Create a public static wrapper class for CreateFaultBookmark & CreateCompletionBookmark methods of ActivityUtilities static class.

---
+++ src/UiPath.Workflow.Runtime/ActivityUtilitiesWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Activities.Runtime;
+
+namespace System.Activities;
+
+public static class ActivityUtilitiesWrapper
+{
+    public static FaultBookmark CreateFaultBookmark(FaultCallback onFaulted, ActivityInstance owningInstance) =>
+        ActivityUtilities.CreateFaultBookmark(onFaulted, owningInstance);
+
+    public static CompletionBookmark CreateCompletionBookmark(CompletionCallback onCompleted, ActivityInstance owningInstance) =>
+        ActivityUtilities.CreateCompletionBookmark(onCompleted, owningInstance);
+}
