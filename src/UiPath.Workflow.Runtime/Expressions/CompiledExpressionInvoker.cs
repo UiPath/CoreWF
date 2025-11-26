0c964539d000671227b98ad66791fe9219c452be
Bogdan Stan <43954969+bogdanStan92@users.noreply.github.com>
2025-11-26 15:00:52 +0200

    add env var flag UIPATH_EXPRESSION_DISABLE_LOCATION_ARGUMENT to disable CreateLocationArgument (#382) (#383)

--- src/UiPath.Workflow.Runtime/Expressions/CompiledExpressionInvoker.cs
+++ src/UiPath.Workflow.Runtime/Expressions/CompiledExpressionInvoker.cs
@@ -9,6 +9,8 @@ namespace System.Activities.Expressions;
 
 public class CompiledExpressionInvoker
 {
+    private static bool DisableLocationArgumentCreation = Environment.GetEnvironmentVariable("UIPATH_EXPRESSION_DISABLE_LOCATION_ARGUMENT") != null;
+
     private static readonly AttachableMemberIdentifier compiledExpressionRootProperty =
         new(typeof(CompiledExpressionInvoker), "CompiledExpressionRoot");
     private static readonly AttachableMemberIdentifier compiledExpressionRootForImplementationProperty =
@@ -226,7 +228,10 @@ public class CompiledExpressionInvoker
         {
             foreach (LocationReference reference in environment.GetLocationReferences())
             {
-                _accessor.CreateLocationArgument(reference, false);
+                if (!DisableLocationArgumentCreation)
+                {
+                    _accessor.CreateLocationArgument(reference, false);
+                }
                 _locationReferences.Add(new InlinedLocationReference(reference, _metadata.CurrentActivity));
             }
         }
